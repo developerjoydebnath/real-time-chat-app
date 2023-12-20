b415fa9a62a871e55f8ce1e3aee9ba4132bd04d6
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2023-12-20 23:14:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Server } from "socket.io";
+
+const io = new Server({cors: {
+    origin: "http://localhost:3000"
+  }});
+
+// declare the online users array 
+let onlineUsers = [];
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('addUser', (userId) => {
+        
+        !onlineUsers.some(user => user.userId === userId) && userId &&
+        onlineUsers.push({
+            socketId: socket.id,
+            userId,
+        })
+
+        io.emit('online-users', onlineUsers)
+    })
+
+    socket.on('addNewUser', (user) => {
+        io.emit('added-new-user', user)
+    })
+
+    socket.on('sendMessage', (messageDetail) => {
+        const receiver = onlineUsers?.find(user => user.userId === messageDetail?.receiverId)
+        if(receiver?.socketId) {
+            io.to(receiver.socketId).emit('get-message', messageDetail)
+        }
+    })
+
+    socket.on('disconnect', () => {
+        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
+        io.emit('online-users', onlineUsers)
+    });
+})
+
+io.listen(5000)
