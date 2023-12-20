dbcedf27a0271078126c5d1cf6d7f34389a74579
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2023-12-20 23:33:36 +0600

    all dk

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 
 const io = new Server({cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
